7f1d838b3133dfc1a9cf0c3107a14bf1b6839971
nykboee <nykboee@gmail.com>
2026-08-27 00:05:21 +0530

    testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Projects } from '@/components/sections/projects'
 import { DesignProcess } from '@/components/sections/design-process'
 import { AiInnovation } from '@/components/sections/ai-innovation'
 import { ThoughtLeadership } from '@/components/sections/thought-leadership'
-import { SomeAppreciations} from '@/components/sections/testimonials'
+import { Testimonials} from '@/components/sections/testimonials'
 import { Resume } from '@/components/sections/resume'
 import { Contact } from '@/components/sections/contact'
 
